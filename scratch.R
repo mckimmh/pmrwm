a86df42efe6ddc045a9492be536fdc95f80fdff4
vcbradley <v.bradley11@gmail.com>
2018-10-12 11:48:56 +0100

    create scratch file

--- scratch.R
+++ scratch.R
@@ -6,8 +6,12 @@ x = rexp(1000, 5)
 # test single step
 stepMH(x, 1, 0.03, sigma_aux = 0.02)
 
-plot(runMH(x, lambda = 2, sigma = 0.03, N = 10000, sigma_aux = NULL)$lambda)
 
+#test with normal M-H
+mh_run = runMH(x, lambda = 2, sigma = 0.03, N = 10000, sigma_aux = NULL)$lambda
+plot(mh_run$lambda)
+
+#test with pseudo M-H
 pmhrun = runMH(x, 2, 0.03, 10000, sigma_aux = 0.1)
-plot(pmhrun)
+plot(pmhrun$lambda)
 hist(pmhrun)
